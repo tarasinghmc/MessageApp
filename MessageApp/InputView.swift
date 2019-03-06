23d0570784bdcd8899e8b2b2e39a8ad5ba5c219c
Tara Singh M C <tarasingmc@gmail.com>
2019-03-07 01:02:25 +0530

    Simple Message App

--- MessageApp/InputView.swift
+++ MessageApp/InputView.swift
@@ -13,25 +13,28 @@ public protocol InputViewDelegate: class {
 }
 
 @IBDesignable class InputView: UIView {
-
+    
     private var containerView = UIView()
     private var textView = UITextView()
     private var sendButton = UIButton()
     private var placeholderLabel = UILabel()
-
+    
     private let space: CGFloat = 12
+    private let maxInputViewHeight: CGFloat = 150
+    private var inputViewHeightConstraint: NSLayoutConstraint!
+    
     
     // Delegate used to notifiy conterverl to send message
     weak var delegate: InputViewDelegate?
     
-   public override init(frame: CGRect) {
+    public override init(frame: CGRect) {
         super.init(frame: frame)
         configureContainerView()
         configureSubViews()
-
+        
     }
     
-   public required init?(coder aDecoder: NSCoder) {
+    public required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         configureContainerView()
         configureSubViews()
@@ -39,11 +42,11 @@ public protocol InputViewDelegate: class {
     
     private func configureContainerView() {
         self.backgroundColor = .white
-     //  self.translatesAutoresizingMaskIntoConstraints = false
-
+        //  self.translatesAutoresizingMaskIntoConstraints = false
+        
         
         containerView.backgroundColor = .white
-        containerView.layer.cornerRadius = 6.0
+        containerView.layer.cornerRadius = 12.0
         containerView.layer.borderColor = UIColor.lightGray.cgColor
         containerView.layer.borderWidth = 1.5
         containerView.layer.masksToBounds = true
@@ -52,11 +55,15 @@ public protocol InputViewDelegate: class {
         self.addSubview(containerView)
         
         
-        containerView.topAnchor.constraint(equalTo: self.topAnchor, constant: space).isActive = true
-        containerView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -space).isActive = true
-        containerView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: space).isActive = true
-        containerView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -space).isActive = true
-        containerView.heightAnchor.constraint(lessThanOrEqualToConstant: 200.0).isActive = true
+        NSLayoutConstraint.activate([
+            containerView.topAnchor.constraint(equalTo: self.topAnchor, constant: space),
+            containerView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -space),
+            containerView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: space),
+            containerView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -space),
+            containerView.heightAnchor.constraint(lessThanOrEqualToConstant: maxInputViewHeight)
+            ])
+        
+        
         
     }
     
@@ -71,48 +78,52 @@ public protocol InputViewDelegate: class {
         textView.isScrollEnabled = false
         containerView.addSubview(textView)
         
-   
         
-        textView.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 0).isActive = true
-        textView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: 0).isActive = true
-        textView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 0).isActive = true
+        
+        NSLayoutConstraint.activate([textView.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 0),
+                                     textView.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: 0),
+                                     textView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: space/2)])
         
         
         // Send Button
         sendButton.addTarget(self, action: #selector(sendMessage(sender:)), for: .touchUpInside)
         sendButton.backgroundColor = .clear
-        sendButton.setImage(UIImage.init(named: "send"), for: .normal)
+        // sendButton.setImage(UIImage.init(named: "sendDeselect"), for: .normal)
+        
+        sendButton.setTitle("Send", for: .normal)
+        sendButton.setTitleColor(.blue, for: .normal)
         sendButton.contentMode = .center
         
         containerView.addSubview(sendButton)
-
+        
         sendButton.translatesAutoresizingMaskIntoConstraints = false
         
-        sendButton.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -8).isActive = true
-        sendButton.leadingAnchor.constraint(equalTo: textView.trailingAnchor, constant: space).isActive = true
-        sendButton.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -space).isActive = true
-        sendButton.widthAnchor.constraint(equalToConstant: 22).isActive = true
-        sendButton.heightAnchor.constraint(equalTo: sendButton.widthAnchor).isActive = true
+        NSLayoutConstraint.activate([sendButton.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -8),
+                                     sendButton.leadingAnchor.constraint(equalTo: textView.trailingAnchor, constant: space),
+                                     sendButton.trailingAnchor.constraint(equalTo: containerView.trailingAnchor, constant: -space),
+                                     sendButton.widthAnchor.constraint(equalToConstant: 44),
+                                     sendButton.heightAnchor.constraint(equalToConstant: 22)])
         
         //PlaceHolder Label
         
-        placeholderLabel.text = " Text Message"
+        placeholderLabel.text = "Text Message"
         placeholderLabel.textColor = .lightGray
         placeholderLabel.font = UIFont.preferredFont(forTextStyle: .body)
         placeholderLabel.translatesAutoresizingMaskIntoConstraints = false
         
         containerView.addSubview(placeholderLabel)
         
-        placeholderLabel.leadingAnchor.constraint(equalTo: containerView.leadingAnchor).isActive = true
-        placeholderLabel.trailingAnchor.constraint(equalTo: sendButton.leadingAnchor, constant: space).isActive = true
-        placeholderLabel.centerYAnchor.constraint(equalTo: textView.centerYAnchor).isActive = true
+        NSLayoutConstraint.activate([placeholderLabel.leadingAnchor.constraint(equalTo: textView.leadingAnchor, constant: space/2),
+                                     placeholderLabel.trailingAnchor.constraint(equalTo: sendButton.leadingAnchor, constant: space),
+                                     placeholderLabel.centerYAnchor.constraint(equalTo: textView.centerYAnchor)])
         
+        print(self.frame.size)
     }
     
-//    public override var intrinsicContentSize: CGSize {
-//       
-//        return CGSize(width: UIView.noIntrinsicMetric, height: height)
-//    }
+    //    public override var intrinsicContentSize: CGSize {
+    //
+    //        return CGSize(width: UIView.noIntrinsicMetric, height: height)
+    //    }
     
     // Send Button Action
     @objc func sendMessage(sender : UIButton) {
@@ -122,7 +133,7 @@ public protocol InputViewDelegate: class {
             
             // Disable send button Action still replay come back from server
             self.sendButton.isUserInteractionEnabled = false
-
+            
             
             //Call SendMessage Delegate, to notifiy Observer
             delegate?.sendMessage(textView.text, completion: { enable in
@@ -140,25 +151,25 @@ public protocol InputViewDelegate: class {
             textView.becomeFirstResponder()
             
             
-
+            
         }
     }
     
-  
+    
     
     /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
+     // Only override draw() if you perform custom drawing.
+     // An empty implementation adversely affects performance during animation.
+     override func draw(_ rect: CGRect) {
+     // Drawing code
+     }
+     */
+    
     
 }
 
 extension InputView: UITextViewDelegate {
-
+    
     
     func textViewDidChange(_ textView: UITextView) {
         
@@ -166,46 +177,60 @@ extension InputView: UITextViewDelegate {
         hidePlaceholderLabel(!textView.text.isEmpty)
         
         // Enable Textview scroll if Inputview height is greter than 200
-        textView.isScrollEnabled = (self.frame.size.height < 200.0) ? false : true
+        textView.isScrollEnabled = (self.frame.size.height < maxInputViewHeight) ? false : true
         
-       /* if self.frame.size.height < 200.0 {
-            textView.isScrollEnabled = false
-            
-//            let fixedWidth = textView.frame.size.width
-//
-//            textView.sizeThatFits(CGSize(width: fixedWidth, height: CGFloat.greatestFiniteMagnitude))
-//
-//            let newSize = textView.sizeThatFits(CGSize(width: fixedWidth, height: CGFloat.greatestFiniteMagnitude))
-//
-//            var newFrame = textView.frame
-//            newFrame.size = CGSize(width: max(newSize.width, fixedWidth), height: newSize.height)
-//            textView.frame = newFrame
-            
-        } else {
-            textView.isScrollEnabled = true
-        }*/
+        /* if self.frame.size.height < maxInputViewHeight {
+         textView.isScrollEnabled = false
+         
+         //            let fixedWidth = textView.frame.size.width
+         //
+         //            textView.sizeThatFits(CGSize(width: fixedWidth, height: CGFloat.greatestFiniteMagnitude))
+         //
+         //            let newSize = textView.sizeThatFits(CGSize(width: fixedWidth, height: CGFloat.greatestFiniteMagnitude))
+         //
+         //            var newFrame = textView.frame
+         //            newFrame.size = CGSize(width: max(newSize.width, fixedWidth), height: newSize.height)
+         //            textView.frame = newFrame
+         
+         } else {
+         textView.isScrollEnabled = true
+         }*/
         
-
+        
     }
+    
     
     
     private func hidePlaceholderLabel(_ hide: Bool) {
-    
+        
         placeholderLabel.isHidden = hide
     }
     
-    /* func textViewDidEndEditing(_ textView: UITextView) {
-     
-     let fixedWidth = textView.frame.size.width
-     
-     textView.sizeThatFits(CGSize(width: fixedWidth, height: CGFloat.greatestFiniteMagnitude))
-     
-     let newSize = textView.sizeThatFits(CGSize(width: fixedWidth, height: CGFloat.greatestFiniteMagnitude))
-     
-     var newFrame = textView.frame
-     newFrame.size = CGSize(width: max(newSize.width, fixedWidth), height: newSize.height)
-     textView.frame = newFrame
-     
-     }*/
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        
+        //        if textView.text.isEmpty {
+        //            inputViewHeightConstraint.constant = 44
+        //            textView.isScrollEnabled = false
+        //        } else {
+        //
+        //            textView.isScrollEnabled = (self.frame.size.height < maxInputViewHeight) ? false : true
+        //        }
+        
+    }
+    
+    //   func textViewDidEndEditing(_ textView: UITextView) {
+    //
+    //     let fixedWidth = textView.frame.size.width
+    //
+    //     textView.sizeThatFits(CGSize(width: fixedWidth, height: CGFloat.greatestFiniteMagnitude))
+    //
+    //     let newSize = textView.sizeThatFits(CGSize(width: fixedWidth, height: CGFloat.greatestFiniteMagnitude))
+    //
+    //     var newFrame = textView.frame
+    //     newFrame.size = CGSize(width: max(newSize.width, fixedWidth), height: newSize.height)
+    //     textView.frame = newFrame
+    //
+    //     }
     
 }
